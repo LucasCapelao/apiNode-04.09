003cc66550f5af4bbb2d5439b3d560e1544785c3
Lucas <capelao.lucas29@gmail.com>
2023-09-04 08:53:28 -0300

    ultima alteracao

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3006;
 
-app.get('/', (req, res) => {
-  res.send('Api retornou')
+app.get('/teste', (req, res) => {
+  res.send('Api retornou');
 })
 
 app.listen(port, () => {
-  console.log(`Api rodando na porta: ${port}`)
+  console.log(`Api rodando na porta: ${port}`);
 })
